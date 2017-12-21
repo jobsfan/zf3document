61cd2e290fa2d3231f87d5c66613abbcc93f7fde
jobsfan <289047960@qq.com>
2017-12-21 11:24:29 +0800

    如果是通配路由，namespace是必须的

--- src/module/Application/config/module.config.php
+++ src/module/Application/config/module.config.php
@@ -33,7 +33,7 @@ return [
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                     ),
                     'defaults' => [
-                        //'__NAMESPACE__' => 'Application\Controller',
+                        '__NAMESPACE__' => 'Application\Controller', //这个好像是必须的
                         'controller' => Controller\IndexController::class,
                         'action'     => 'index',
                     ],
