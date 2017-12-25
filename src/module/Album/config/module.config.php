f0af299113bbf4f5d84a3491f09542287244fbcb
jobsfan <289047960@qq.com>
2017-12-25 17:46:21 +0800

    500错误

--- src/module/Album/config/module.config.php
+++ src/module/Album/config/module.config.php
@@ -1,6 +1,7 @@
 <?php
 namespace Album;
 
+use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
@@ -9,6 +10,26 @@ return [
             Controller\AlbumController::class => InvokableFactory::class,
         ],
     ],
+    
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    
     'view_manager' => [
         'template_path_stack' => [
             'album' => __DIR__ . '/../view',
