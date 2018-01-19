593607664cfa6fa0a176b4b31dca7035532d2ae5
jobsfan <289047960@qq.com>
2018-01-19 18:05:04 +0800

    merged configuration备份一个在module.php里面

--- src/module/Blog/src/Module.php
+++ src/module/Blog/src/Module.php
@@ -1,10 +1,36 @@
 <?php
 namespace Blog;
 
+// use Zend\ModuleManager\ModuleEvent;
+// use Zend\ModuleManager\ModuleManager;
+
 class Module
 {
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
     }
+    
+    /* public function init(ModuleManager $moduleManager)
+    {
+        $events = $moduleManager->getEventManager();
+        
+        // Registering a listener at default priority, 1, which will trigger
+        // after the ConfigListener merges config.
+        $events->attach(ModuleEvent::EVENT_MERGE_CONFIG, [$this, 'onMergeConfig']);
+    }
+    
+    public function onMergeConfig(ModuleEvent $e)
+    {
+        $configListener = $e->getConfigListener();
+        $config         = $configListener->getMergedConfig(false);
+        
+        // Modify the configuration; here, we'll remove a specific key:
+        if (isset($config['some_key'])) {
+            unset($config['some_key']);
+        }
+        
+        // Pass the changed configuration back to the listener:
+        $configListener->setMergedConfig($config);
+    } */
 }
