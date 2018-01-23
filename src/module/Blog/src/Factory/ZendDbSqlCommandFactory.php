f0794a4cff98050b36e58b9a9aef6e0bfae51c95
jobsfan <289047960@qq.com>
2018-01-23 18:12:47 +0800

    多大

--- src/module/Blog/src/Factory/ZendDbSqlCommandFactory.php
+++ src/module/Blog/src/Factory/ZendDbSqlCommandFactory.php
@@ -3,13 +3,17 @@ namespace Blog\Factory;
 
 use Interop\Container\ContainerInterface;
 use Blog\Model\ZendDbSqlCommand;
-use Zend\Db\Adapter\AdapterInterface;
+//use Zend\Db\Adapter\AdapterInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\Db\Adapter\Adapter;
 
 class ZendDbSqlCommandFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        return new ZendDbSqlCommand($container->get(AdapterInterface::class));
+        $config = $container->get('config');
+        $tutorialDb = $config['tutorial'];
+        $dbAdapter = new Adapter($tutorialDb);
+        return new ZendDbSqlCommand($dbAdapter); //$container->get(AdapterInterface::class)
     }
 }
