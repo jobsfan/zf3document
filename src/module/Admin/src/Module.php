90ebf748079a75a34b4528ecdd672a319ebf941f
jobsfan <289047960@qq.com>
2018-02-24 14:14:36 +0800

    如果要添加统一的路由，也就是controller和action都是变量，在这个模块下，只需要遵循统一的url就无需一一添加路由，那么必须要加ModuleRouteListener

--- src/module/Admin/src/Module.php
+++ src/module/Admin/src/Module.php
@@ -2,9 +2,18 @@
 namespace Admin;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
 
 class Module implements ConfigProviderInterface
 {
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+    
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
