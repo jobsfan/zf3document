07280f14f0292321a77bd646ecb3ff94965ca4ff
jobsfan <289047960@qq.com>
2018-02-24 14:10:15 +0800

    开始会员模块的逻辑，本来按照我自己的习惯是会命名成member的，但是考虑到大家的习惯，就改成了admin，为了简化流程，避免平凡的改路由，就设置成了统一的路由格式

--- src/module/Admin/config/module.config.php
+++ src/module/Admin/config/module.config.php
@@ -2,13 +2,38 @@
 namespace Admin;
 
 use Zend\ServiceManager\Factory\InvokableFactory;
+use Zend\Router\Http\Segment;
 
 return [
     'controllers' => [
         'factories' => [
-            //Controller\AlbumController::class => InvokableFactory::class,
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+        'invokables' => [
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
         ],
     ],
+    
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/admin/[:controller[/:action]]',
+                    'constraints' => array(
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]',
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]',
+                    ),
+                    'defaults' => [
+                        '__NAMESPACE__' => 'Application\Controller', //这个好像是必须的
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    
     'view_manager' => [
         'template_path_stack' => [
             'admin' => __DIR__ . '/../view',
