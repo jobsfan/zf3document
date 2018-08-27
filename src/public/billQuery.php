6b7d2d9a3471c9d5ec28951000735c75f7da769d
root <289047960@qq.com>
2018-08-27 05:14:59 +0000

    billQuery x

--- src/public/billQuery.php
+++ src/public/billQuery.php
@@ -119,8 +119,7 @@ class billQuery
     //日本专线逸迅达的处理逻辑 http://www.tdlexp.com/cgi-bin/GInfo.dll?EmmisTrack
     public function tdlexp($billNo) //测试单号 3334747124
     {
-        $rawHtml = $this->curlRemote('http://www.tdlexp.com/cgi-bin/GInfo.dll?EmmisTrack', 'post', array('w' => 'tdlexp', 'ntype' => 1000, 'cno' => $billNo));
-        echo $rawHtml;exit;
+        $rawHtml = $this->curlRemote('http://www.tdlexp.com/cgi-bin/GInfo.dll?EmmisTrack', 'post', 'w=tdlexp&ntype=1000&cno='.$billNo);
         preg_match('%(<table.*?class=\'trackListTable\'>.*?</table>)<br>%is', $rawHtml, $matches);
         if (isset($matches[1]) && $matches[1])
         {
